4b19938ee95a4b7659b2bdeca570a5df1cb94c81
Urs Hanselmann <6864721+urshanselmann@users.noreply.github.com>
2025-06-04 20:37:58 +0200

    Fix example lexer and tests

--- examples/cmake/tests.cpp
+++ examples/cmake/tests.cpp
@@ -2,17 +2,24 @@
 #include "doctest.h"
 #include "essl_strip.h"
 #include <string>
+#include <cstring>
 
 DOCTEST_TEST_CASE(test_strip_310) {
     const char *shader = "#version 310 es\nprecision mediump float;\nmediump vec4 color;";
+    size_t len = std::strlen(shader);
+    std::string buf(shader, len);
+    buf.append(YYMAXFILL + 1, '\0');
     std::string out;
-    essl_strip(shader, out);
+    essl_strip(buf.c_str(), out);
     DOCTEST_CHECK(out == "#version 430 core\nvec4 color;");
 }
 
 DOCTEST_TEST_CASE(test_strip_100) {
     const char *shader = "#version 100\nprecision mediump float;\nmediump vec4 color;";
+    size_t len = std::strlen(shader);
+    std::string buf(shader, len);
+    buf.append(YYMAXFILL + 1, '\0');
     std::string out;
-    essl_strip(shader, out);
-    DOCTEST_CHECK(out == "#version 100\nprecision mediump float;\nmediump vec4 color;");
+    essl_strip(buf.c_str(), out);
+    DOCTEST_CHECK(out == "#version 100\n\nprecision mediump float;\nmediump vec4 color;");
 }
